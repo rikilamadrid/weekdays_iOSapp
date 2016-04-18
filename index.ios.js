5c1dd4308ee1e3427a36ee9eeff305daff01e4b1
rikilamadrid <riki.lamadrid@gmail.com>
2016-04-18 12:48:23 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+//Import some code we need
+var Moment = require('moment');
+var React = require('react-native');
+var AppRegistry = React.AppRegistry;
+var Text = React.Text;
+var View = React.View;
+var StyleSheet = React.StyleSheet;
+var DayItem = require('./src/day-item');
+
+//var DAYS = ['Sonnstag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
+
+// Create a react component
+var Weekdays = React.createClass({
+    render: function() {
+        return <View style={styles.container}>
+            {this.days()}
+        </View>
+    },
+    days: function() {
+        var daysItems = [];
+
+        for(var i = 0; i < 7; i++){
+            var day = Moment().add(i, 'days').format('dddd');
+            daysItems.push(
+                <DayItem day={day} daysUntil={i} />
+            )
+        }
+
+        return daysItems
+
+        //return DAYS.map(function(day){
+            //called 7 times, for each day of the week
+            //return <DayItem day={day} />
+        //});
+    }
+});
+
+// Style the react component
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center', // y axis
+        alignItems: 'center' //x axis
+    }
+});
+
+//Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
+    return Weekdays
+});
